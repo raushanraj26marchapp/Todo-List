09ea4b5ed050d17dc801c1e3aa4220c6fad948f6
raushanraj26marchapp <78207280+raushanraj26marchapp@users.noreply.github.com>
2022-02-09 16:02:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,23 +20,21 @@ function App() {
 
   const deleteItem = (id) => {
     console.log("deleted")
-    alert("List Deleted")
-    setItem((oldItem) => {
-      return oldItem.filter((arrElem, index) => {
-        return index != id;
+    setItem((oldItem)=>{
+      return oldItem.filter((arrElem, index)=>{
+        return index!=id;
       })
     })
   }
 
   return (
-    <div className='container'>
-      <div className='heading'>
-        <center><h1>My To Do List</h1></center><br />
-        <div className='mainf'>
-          <input id="input" type='text' value={inputList} placeholder='add list here' onChange={itemEvent} />
+    <>
+      <div className='header'>
+        <h1>Todo List</h1>
+        <div className='input'>
+        <input type='text' value={inputList} placeholder='add list here' onChange={itemEvent} />
 
-          <button id="add" onClick={listOfItems}>Add</button>
-
+          <button id='add' onClick={listOfItems}>Add</button>
           <ol>
             {item.map((ItemVal, index) => {
               return <Todos key={index} id={index} onSelect={deleteItem} text={ItemVal} />
@@ -45,7 +43,8 @@ function App() {
 
         </div>
       </div>
-    </div>
+      
+    </>
   );
 }
 
