064ba4a6aa4cdd81afc5235e4e71df1ef49c239c
raushanraj26marchapp <78207280+raushanraj26marchapp@users.noreply.github.com>
2022-02-09 16:02:55 +0530

    Update Todos.js

--- src/Todos.js
+++ src/Todos.js
@@ -5,8 +5,8 @@ const Todos = (props) => {
 
     return (
         <>
-            <div className='todo'>
-                <button id="del" onClick={() => {
+            <div className='del'>
+                <button id='delb' onClick={()=>{
                     props.onSelect(props.id)
                 }}>Delete</button>
                 <li>{props.text}</li>
